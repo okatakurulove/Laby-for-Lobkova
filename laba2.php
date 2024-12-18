86efe3831d34ec15d81003ecf3cd8ffdd7cc64f6
britneysmokescrack <57794211+okatakurulove@users.noreply.github.com>
2024-12-18 22:55:53 +0300

    Update laba2.php

--- laba2.php
+++ laba2.php
@@ -1,21 +1,42 @@
 <?php
 $my_info = [
-    "name" => "Артём",
-    "surname" => "Фомин",
-    "age" => 18
+    'first_name' => 'Артём',
+    'last_name' => 'Фомин',
+    'age' => 18
 ];
-
 
 $friend_info = [
-    "name" => "Вадим",
-    "surname" => "Борисов",
-    "age" => 19
+    'first_name' => 'Вадим',
+    'last_name' => 'Борисов',
+    'age' => 20
 ];
 
+$my_age = $my_info['age'];
+$friend_age = $friend_info['age'];
 
-$my_age = $my_info["age"];
-$friend_age = $friend_info["age"];
+if ($my_age > $friend_age) {
+    echo "Я старше: $my_age летn";
+} elseif ($my_age < $friend_age) {
+    echo "Мой друг старше: $friend_age летn";
+} else {
+    echo "Мы одного возраста: $my_age летn";
+}
 
-echo "Мой возраст: " . $my_age . " лет.\n";
-echo "Возраст моего друга: " . $friend_age . " лет.";
+$name_length = strlen($my_info['first_name']);
+$friend_name_length = strlen($friend_info['first_name']);
+
+if ($name_length > $friend_name_length) {
+    echo "Мое имя длиннее: '{$my_info['first_name']}' ($name_length символов)n";
+} elseif ($name_length < $friend_name_length) {
+    echo "Имя моего друга длиннее: '{$friend_info['first_name']}' ($friend_name_length символов)n";
+} else {
+    echo "Наши имена одинаковой длины: $name_length символовn";
+}
+
+$day = date('j');
+$last_day_of_month = date('t');
+
+$days_remaining = $last_day_of_month - $day;
+
+echo "До конца месяца осталось $days_remaining дней.n";
 ?>
